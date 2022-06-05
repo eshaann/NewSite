7374214c70bce42c74b79bbbe5867f30f1865a05
Eshaan Nair <eshaann@nycstudents.net>
2022-06-05 00:38:26 -0400

    Add files via upload
    REFACTORED VERSION 
    -Shop needs to be fixed again tho

---
+++ LargeProject/index.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/shop')
+def shop():
+    return render_template('shop.html')
+
+@app.route('/postproduct')
+def postproduct():
+    return render_template('postproduct.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/thankyou')
+def thankyou():
+    first = request.args.get('firstname')
+    return render_template('thankyou.html', first = first)
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+if __name__ == '__main__':
+    app.run(debug = True)
